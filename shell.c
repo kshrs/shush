b2ee15eafe03f8728c8bb39236827dfb8953166d
kishor <kishorjsk2006@gmail.com>
2026-04-13 18:19:29 +0530

    Rework of the basic shell functionalities.

--- shell.c
+++ shell.c
@@ -1,105 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
 
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
-#define MINBUFFER 3
+#ifndef EXIT_SUCCESS
+    #define EXIT_SUCCESS 0
+#endif
 
-void shush_loop();
-char* shush_readline();
-char** shush_splitargs(char* line);
-int shush_execute(char** args);
+#ifndef EXIT_FAILURE
+    #define EXIT_FAILURE 1
+#endif
 
+#ifndef NO_ARGS_PASSED
+ #define NO_ARGS_PASSED 55
+#endif
+
+void shush_mainloop();
+char* readline();
+char** splitargs(char* line);
+int execute(char** args);
+void display_prompt(int status);
+
+void exit_handler(int signal);
 
 int main(int argc, char** argv) {
-    // 1. Load some configuration if present
+    signal(SIGINT, exit_handler);
+
+    // 1. config
     
-    // 2. Load Eval loop
-    shush_loop();
+    // 2. mainloop
+    shush_mainloop();
+
+    // 3. clear mem
     
-    // 3. Free up memory at the end
-    printf("Exiting shell\n\n\n");
     return EXIT_SUCCESS;
 }
 
-void shush_loop() {
+
+void shush_mainloop() {
     char* line;
     char** args;
-    int status;
+    int status = 0;
+    
+    while(1) {
+        // 0. display prompt
+        display_prompt(status);
 
-    do {
-        printf(">>> ");
-        line = shush_readline();
-        if (!line) {
-            printf("Error reading line from stdin\n");
-        }
-        args = shush_splitargs(line);
-        status = shush_execute(args);
-    } while(status);
+        // 1. readline
+        line = readline();
 
+        // 2. splitargs
+        args = splitargs(line);
+        
+        // 3. execute
+        status = execute(args);
+    } 
+    
     free(line);
     free(args);
 }
 
-char* shush_readline() {
-    size_t current_size = MINBUFFER;
-    void* realloc_status = NULL;
-    char* line = malloc(current_size);
+#define INIT_READLINE_BUFSIZE 50;
+char* readline() {
+    int capacity = INIT_READLINE_BUFSIZE;
+    char* line = (char*) malloc(sizeof(char*) * capacity);
+
+    if (!line) {
+        fprintf(stderr, "Failed to allocate memory to (char* line)\n");
+        exit(-1);
+    }
+
     int c;
     int len = 0;
 
-    while( ((c = getchar()) != EOF) && c != '\n' ) {
-        if (len == current_size-1) {
-            current_size *= 2;
-            realloc_status = realloc(line, current_size);
-            if (!realloc_status) {
-                return NULL;
+    while((c=getchar()) != EOF && c != '\n') {
+        if (len == (capacity-1)) {
+            capacity = 2 * capacity;
+            line = (char*) realloc(line, sizeof(char*) * capacity);
+
+            if (!line) {
+                fprintf(stderr, "Failed to reallocate memory to (char* line)\n");
+                exit(-1);
             }
         }
-        *(line + len) = c;
-        len++;
+
+        line[len++] = c;
     }
-    *(line + len) = '\0';
-    return line;
+    line[len++] = '\0';
+
+    return (char *) line;
 }
 
-char** shush_splitargs(char* line) {
-    char* delim = " ";
-    char* token = strtok(line, delim);
-    char** tokens = NULL;
+char** splitargs(char* line) {
     int count = 0;
 
+    char** tokens = malloc(sizeof(char*) * (count + 1));
+    const char* delim = " ;";
+
+    if (!*line) {
+        return NULL;
+    }
+
+    char* token = strtok(line, delim);
     while(token != NULL) {
-        if (tokens == NULL) {
-            tokens = malloc(sizeof(char*));
-        } else {
-            realloc(tokens, sizeof(char*) * (count + 1));
-        }
-
-        tokens[count] = strdup(token);
+        *(tokens + count) = token;
         count++;
-
+        tokens = (char**) realloc(tokens, sizeof(char*) * (count + 1));
         token = strtok(NULL, delim);
     }
-
-    char* temp = *tokens;
-    // for(int i=1; i<=count; ++i) {
-    //     printf("Token: %s\n", temp);
-    //     temp = *(tokens + i);
-    // }
-
-    return tokens;
+    *(tokens + count) = token;
+    return (char**) tokens;
 }
 
-int shush_execute(char** args) {
-    if (args[0] == NULL) {
-        return 0;
+void exit_handler(int sig) {
+    exit(0);
+}
+
+int execute(char** args) {
+    if (!args) {
+        return NO_ARGS_PASSED;
     }
+
     if (strcmp(args[0], "exit") == 0) {
-        return 0;
+        printf("Exiting shush....\n");
+        exit(0);
     }
-    return 1;
 
+    return 0;
 }
 
+void display_prompt(int status) {
+    const char* prompt = "> ";
+    char nil_flag[6] = "nil";
+
+    switch(status) {
+        case NO_ARGS_PASSED:
+            strcat(nil_flag, prompt);
+            write(STDOUT_FILENO, nil_flag, strlen(nil_flag));
+            break;
+            
+        default:
+            write(STDOUT_FILENO, prompt, strlen(prompt));
+    }
+}
